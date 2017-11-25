4891310555e07314c4f14b510a51a567420b45fa
Vlad Buhoci <buhoci.vlad@gmail.com>
2017-11-25 14:00:57 +0200

    GuardAI

--- JailBreak/Assets/Scripts/GuardAI.cs
+++ JailBreak/Assets/Scripts/GuardAI.cs
@@ -1,22 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GuardAI : MonoBehaviour
 {
-    /// <summary>
-    /// Commit test.
-    /// </summary>
+    public GameObject player;
+
+    private NavMeshAgent navMeshAgent;
 
     // Use this for initialization
     void Start()
     {
-
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        navMeshAgent.SetDestination(player.gameObject.transform.position);
     }
 }
