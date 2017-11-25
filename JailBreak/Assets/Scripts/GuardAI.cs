e54d9e5cf477538005a75a18369aab9dd0fd0632
Vlad Buhoci <buhoci.vlad@gmail.com>
2017-11-25 12:18:38 +0200

    First commit.

---
+++ JailBreak/Assets/Scripts/GuardAI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardAI : MonoBehaviour
+{
+    /// <summary>
+    /// Commit test.
+    /// </summary>
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
