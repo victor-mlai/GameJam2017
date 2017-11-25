45fe14bf546ff13d048b8f8f1fab97149ce6c97f
Cosmina Alexandru <nyx_cosmina@yahoo.ro>
2017-11-25 17:02:08 +0200

    Added Main Menu and Game Over

---
+++ JailBreak/Assets/Scripts/ButtonAudio.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAudio : MonoBehaviour {
+
+    AudioManager audioManager;
+
+	// Use this for initialization
+	void Start ()
+    {
+        audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+	}
+	
+    public void ButtonHover()
+    {
+        if (audioManager != null)
+        {
+            audioManager.Play("ButtonHover");
+        }
+    }
+
+    public void PlayGame()
+    {
+        Application.LoadLevel("Nyx");
+    }
+
+    public void MainMenu()
+    {
+        Application.LoadLevel("Main Menu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
